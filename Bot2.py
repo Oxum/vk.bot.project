6510619f5ea7b657ca5c709e38dbd1ff6768d355
My Name <Plausss@bk.ru>
2019-08-05 20:10:37 +0300

    added gitignore (IDEA settings)

--- Bot2.py
+++ Bot2.py
@@ -1,35 +1,62 @@
 import vk_api
-import random
-import time
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 
-#Наш токен полученный с группы вк
-token = "0fee6d3eea0e4f7ea1950c5d930230fcce3f19eeb6cb68f7f1f04bf482b63db3014f3276326a186668f8d"
+vk_session = vk_api.VkApi(
+    token='0fee6d3eea0e4f7ea1950c5d930230fcce3f19eeb6cb68f7f1f04bf482b63db3014f3276326a186668f8d')  # токен вашей группы
+vk = vk_session.get_api()
+longpoll = VkBotLongPoll(vk_session, '180495402')  # цифровой id вашей группы
 
 
-vk = vk_api.VkApi(token=token)
+def main():
+    keyboard1 = VkKeyboard(one_time=False)  # создаем клавиатуру. False - клавиатура после нажатия не будет закрываться. True - будет.
+    keyboard1.add_button('Помощь', color=VkKeyboardColor.POSITIVE)  # цвета. POSITIVE - Зелёный. NEGATIVE - Красный. PRIMARY - Синий
+    keyboard1.add_button('Привет', color=VkKeyboardColor.POSITIVE)  # цвета. POSITIVE - Зелёный. NEGATIVE - Красный. PRIMARY - Синий
+    keyboard1.add_line()  # добавляем ещё одну строку для клавиатуры
+    keyboard1.add_button('Закрыть клавиатуру', color=VkKeyboardColor.NEGATIVE)
 
-vk._auth_token()
+    helpmsg = ''' Помощь ответ! '''
+    hellomsg = ''' Привет! '''
+    Menusmg = ''' Вы бери из списка, что тебе нужно: '''
 
-#У кнопок может быть один из 4 цветов:
-#1. primary — синяя кнопка, обозначает основное действие. #5181B8
-#2. default — обычная белая кнопка. #FFFFFF
-#3. negative — опасное действие, или отрицательное действие (отклонить, удалить и тд). #E64646
-#4. positive — согласиться, подтвердить. #4BB34B
+    while True:  # бесконечная проверка на получение событий в группе (если поймаем ошибку)
+        try:  # ловим ошибку
+            for event in longpoll.listen():
 
-while True:
-    try:
-    #Присоединяемся к серверу
-        messages = vk.method("messages.getConversations", {"offset": 0, "count": 20, "filter": "unanswered"})
-        if messages["count"] >= 1:
-            id = messages["items"][0]["last_message"]["from_id"]
-            body = messages["items"][0]["last_message"]["text"]
-    #Задаем сообщение, которое должен ввести пользователь!(С маленькой буквы!)
-            if body.lower() == "привет":
-    #Что мы будем отвечать на это слово!
-                vk.method("messages.send", {"peer_id": id, "message": "Привет!", "random_id": random.randint(1, 2147483647)})
-            elif body.lower() == "кто я?":
-                vk.method("messages.send", {"peer_id": id, "message": "Ты хороший человек", "random_id": random.randint(1, 2147483647)})
+                if event.type == VkBotEventType.MESSAGE_NEW and event.object.text.lower() == 'помощь':
+                    vk.messages.send(peer_id=event.object.peer_id,
+                                     message=helpmsg,
+                                     random_id=0)
+                # отправляем пользователю пзаготовленное сообщение hellomsg
 
-    except Exception as E:
-        time.sleep(1)
-#Доделать клавиатуру и научить отправлять аудио
+                if event.type == VkBotEventType.MESSAGE_NEW and event.object.text.lower() == 'привет':
+                    vk.messages.send(peer_id=event.object.peer_id,
+                                     message=hellomsg,
+                                     random_id=0)
+                # отправляем пользователю заготовленное сообщение hellomsg.
+
+                if event.type == VkBotEventType.MESSAGE_NEW and event.object.text.lower() == 'начать':
+                    vk.messages.send(peer_id=event.object.peer_id,
+                                     message=Menusmg,
+                                     keyboard=keyboard1.get_keyboard(),
+                                     random_id=0)
+                # По команде начать (вызываем клавиатуру)
+
+                if event.type == VkBotEventType.MESSAGE_NEW and event.object.text.lower() == 'закрыть клавиатуру':
+                    vk.messages.send(peer_id=event.obj.peer_id,
+                                     message='Клавиатура закрыта, чтобы заново вызвать введите: начать',
+                                     random_id=0,
+                                     keyboard=keyboard1.get_empty_keyboard())
+                # отправляем пользователю пустую клавиатуру и сообщение о успешном закрытии
+
+                if event.type == VkBotEventType.MESSAGE_NEW and event.object.text.lower() == 'тро':
+                    vk.messages.send(user_id=460193519,
+                                     message='Вызвали')
+                # отправляем пользователю заготовленное сообщение hellomsg.
+
+        except Exception as e:
+            print('')  # выводим сообщение о том что получили ошибку
+
+
+if __name__ == '__main__':
+    main()
--- main.py
+++ main.py
@@ -1,28 +1,64 @@
 import vk_api
-import random
-import time
-token = "0fee6d3eea0e4f7ea1950c5d930230fcce3f19eeb6cb68f7f1f04bf482b63db3014f3276326a186668f8d"
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+
+vk_session = vk_api.VkApi(
+    token='0fee6d3eea0e4f7ea1950c5d930230fcce3f19eeb6cb68f7f1f04bf482b63db3014f3276326a186668f8d')  # токен вашей группы
+vk = vk_session.get_api()
+longpoll = VkBotLongPoll(vk_session, '180495402')  # цифровой id вашей группы
 
 
-vk = vk_api.VkApi(token=token)
+def main():
+    keyboard1 = VkKeyboard(one_time=False)  # создаем клавиатуру. False - клавиатура после нажатия не будет закрываться. True - будет.
+    keyboard1.add_button('Помощь', color=VkKeyboardColor.POSITIVE)  # цвета. POSITIVE - Зелёный. NEGATIVE - Красный. PRIMARY - Синий
+    keyboard1.add_button('Привет', color=VkKeyboardColor.POSITIVE)  # цвета. POSITIVE - Зелёный. NEGATIVE - Красный. PRIMARY - Синий
+    keyboard1.add_line()  # добавляем ещё одну строку для клавиатуры
+    keyboard1.add_button('Закрыть клавиатуру', color=VkKeyboardColor.NEGATIVE)
 
-vk._auth_token()
+    helpmsg = ''' Помощь ответ! '''
+    hellomsg = ''' Привет! '''
+    Menusmg = ''' Вы бери из списка, что тебе нужно: '''
+
+    while True:  # бесконечная проверка на получение событий в группе (если поймаем ошибку)
+        try:  # ловим ошибку
+            for event in longpoll.listen():
+
+                if event.type == VkBotEventType.MESSAGE_NEW and event.object.text.lower() == 'помощь':
+                    vk.messages.send(peer_id=event.object.peer_id,
+                                     message=helpmsg,
+                                     random_id=0)
+                # отправляем пользователю пзаготовленное сообщение hellomsg
+
+                if event.type == VkBotEventType.MESSAGE_NEW and event.object.text.lower() == 'привет':
+                    vk.messages.send(peer_id=event.object.peer_id,
+                                     message=hellomsg,
+                                     random_id=0)
+                # отправляем пользователю заготовленное сообщение hellomsg.
+
+                if event.type == VkBotEventType.MESSAGE_NEW and event.object.text.lower() == 'начать':
+                    vk.messages.send(peer_id=event.object.peer_id,
+                                     message=Menusmg,
+                                     keyboard=keyboard1.get_keyboard(),
+                                     random_id=0)
+                # По команде начать (вызываем клавиатуру)
+
+                if event.type == VkBotEventType.MESSAGE_NEW and event.object.text.lower() == 'закрыть клавиатуру':
+                    vk.messages.send(peer_id=event.obj.peer_id,
+                                     message='Клавиатура закрыта, чтобы заново вызвать введите: начать',
+                                     random_id=0,
+                                     keyboard=keyboard1.get_empty_keyboard())
+                # отправляем пользователю пустую клавиатуру и сообщение о успешном закрытии
+
+                if event.type == VkBotEventType.MESSAGE_NEW and event.object.text.lower() == 'тро':
+                    vk.messages.send(user_id=460193519,
+                                     message='Вызвали')
+                # отправляем пользователю заготовленное сообщение hellomsg.
+
+        except Exception as e:
+            print('')  # выводим сообщение о том что получили ошибку
 
 
+if __name__ == '__main__':
+    main()
 
-while True:
-    try:
-        messages = vk.method("messages.getConversations", {"offset": 0, "count": 20, "filter": "unanswered"})
-        if messages["count"] >= 1:
-            id = messages["items"][0]["last_message"]["from_id"]
-            body = messages["items"][0]["last_message"]["text"]
-            if body.lower() == "привет":
-                vk.method("messages.send", {"peer_id": id, "message": "Привет!", "random_id": random.randint(1, 2147483647)})
-            elif body.lower() == "":
-                vk.method("messages.send", {"peer_id": id, "message": "ты хороший человек", "random_id": random.randint(1, 2147483647)})
-
-            else:
-                vk.method("messages.send", {"peer_id": id, "message": "я не знаю что значит " + str(body.lower()), "random_id": random.randint(1, 2147483647)})
-    except Exception as E:
-        time.sleep(1)
 
